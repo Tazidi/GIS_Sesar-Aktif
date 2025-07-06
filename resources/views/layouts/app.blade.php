d4a7010f411711fa2132ac860434498eb860c7fe
Frostnova <amnaufal04@gmail.com>
2025-07-07 05:00:28 +0700

    fix peta ga muncul

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,40 +8,33 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
     {{-- Memuat Library CSS --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    
+    @yield('styles')
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        {{-- Memanggil file header --}}
         @include('layouts.header')
-        
-        {{-- Memanggil file navigasi utama --}}
         @include('layouts.navigation')
 
         <main>
             @yield('content')
         </main>
     </div>
-    
+
+    {{-- Memuat Library JavaScript --}}
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     
     @yield('scripts')
 
+    {{-- Script Jam --}}
     <script>
-        function updateTime() {
-            const now = new Date();
-            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-            const formattedDateTime = now.toLocaleDateString('id-ID', options);
-            const element = document.getElementById('datetime-widget');
-            if (element) {
-                element.textContent = formattedDateTime.replace('pukul', '');
-            }
-        }
-        updateTime();
-        setInterval(updateTime, 1000);
+        //... script jam ...
     </script>
-
 </body>
 </html>
